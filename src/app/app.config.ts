f578bb2aa73375f24354252e69316b960b8fa823
Nael Muhamed <ozymandias.ll.kmt@gmail.com>
2025-08-26 21:43:19 +0300

    Installing PrimeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,36 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+    // PrimeNG
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        options: {
+          darkModeSelector: '.dark',
+        },
+        preset: Aura,
+      },
+    }),
+  ],
 };
